55ce2614977b7efaae30e2af17d3aa0188668559
Pavel <Pasha.Kycher89@gmail.com>
2026-01-28 12:25:35 +0200

    fix9

--- client/src/pages/owner/DelRepord.jsx
+++ client/src/pages/owner/DelRepord.jsx
@@ -49,7 +49,7 @@ const DelRepord = () => {
                 {reportsData.map((report) => (
                     <div
                         key={report._id}
-                        className='flex flex-col md:flex-row items-start gap-6 bg-white p-6 rounded-xl shadow-md w-full'>
+                        className='flex flex-col md:flex-row items-start gap-6 bg-white p-6 rounded-xl shadow-md w-full overflow-hidden'>
                         <img
                             src={report.image}
                             alt={report.title}
